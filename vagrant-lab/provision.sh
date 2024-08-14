892d5db0ac957fd4a1f925396821c38cc2044410
Gabriel Augusto <gabrielaugusto753@gmail.com>
2024-08-14 15:20:45 -0300

    initial commit

---
+++ vagrant-lab/provision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "Instalando Apache e executando setup..."
+sudo apt update
+sudo apt install -y apache2
+cp -r /vagrant/html/* /var/www/html
+service httpd start
+echo "............................. httpd start .............."
